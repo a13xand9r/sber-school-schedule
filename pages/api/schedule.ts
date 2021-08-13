2ac7d0293a0b305b2c8f91f514872b454aab7faa
Aleksandr <a13x.student@yandex.ru>
2021-08-13 16:10:14 +0300

    mongoDB test

--- pages/api/schedule.ts
+++ pages/api/schedule.ts
@@ -1,13 +1,13 @@
 import { ScheduleType } from './../../store';
-import { changeSchedule, getSchedule } from './../../scenario/scheduleData';
+import { changeSchedule, getSchedule } from '../../scenario/dataBase';
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<ScheduleType>) {
   console.log('schedule request, method', req.method, 'userId: ', req.query.id)
   if (req.method === 'GET'){
-    res.status(200).json(getSchedule(req.query.id as string))
+    res.status(200).json(await getSchedule(req.query.id as string))
   }
   if (req.method === 'POST'){
-    res.status(200).json(changeSchedule(req.query.id as string, req.body))
+    res.status(200).json(await changeSchedule(req.query.id as string, req.body))
   }
 }
---
+++ scenario/dataBase.ts
@@ -0,0 +1,55 @@
+import { MongoClient } from 'mongodb'
+import { ScheduleType } from '../store'
+
+const client = new MongoClient('mongodb+srv://school_schedule:123qwerty@cluster0.siwn0.mongodb.net/schedule')
+
+export const start = async () => {
+  try {
+    await client.connect()
+    console.log('MongoDB connected')
+  } catch (err) {
+    console.log(err)
+  }
+}
+start()
+
+const scheduleDB = client.db().collection('schedule')
+const emptySchedule = {
+  'Понедельник': null,
+  'Вторник': null,
+  'Среда': null,
+  'Четверг': null,
+  'Пятница': null,
+  'Суббота': null,
+}
+
+export const getSchedule = async (userId: string): Promise<ScheduleType> => {
+  try {
+    const user = await scheduleDB.findOne({ userId })
+    console.log(user)
+    if (user) {
+      return user.schedule
+    } else {
+      return emptySchedule
+    }
+  } catch (err) {
+    console.log('mongoDB error: ', err)
+    return emptySchedule
+  }
+}
+
+export const changeSchedule = async (userId: string, newSchedule: ScheduleType): Promise<ScheduleType> => {
+  const user = await scheduleDB.findOne({ userId })
+  console.log(user)
+  try {
+    if (user) {
+      await scheduleDB.updateOne({ userId }, { userId, schedule: newSchedule })
+    } else {
+      scheduleDB.insertOne({ userId, schedule: newSchedule })
+    }
+    return newSchedule
+  } catch (err) {
+    console.log('mongoDB error: ', err)
+    return emptySchedule
+  }
+}
