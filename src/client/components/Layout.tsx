eaeed997f1753715e47ced64fca6af2aa635a14e
Aleksandr <a13x.student@yandex.ru>
2021-08-25 10:44:02 +0300

    unused import deleted

--- src/client/components/Layout.tsx
+++ src/client/components/Layout.tsx
@@ -1,6 +1,5 @@
 import React, { ReactNode } from 'react'
 import { DeviceThemeProvider } from '@sberdevices/plasma-ui/components/Device'
-import { sberBox } from '@sberdevices/plasma-tokens'
 
 export const Layout = ({ children }: { children: ReactNode }) => {
   return <>
