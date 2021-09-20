24ef82e5e98ed3ca5f32b98c377aa1b7e721cc81
Aleksandr <a13x.student@yandex.ru>
2021-09-20 11:18:22 +0300

    changed task page appearance

--- src/client/components/Task.tsx
+++ src/client/components/Task.tsx
@@ -1,14 +1,23 @@
-import { Body1, Button } from '@sberdevices/plasma-ui'
+import { Body1, Button, Card, CardBody, CardContent, TextBox } from '@sberdevices/plasma-ui'
 import React, { FC } from 'react'
 import style from '../../../styles/schedule.module.css'
 import { HomeTaskType } from '../../types'
 
-export const Task: FC<PropsType> = ({showTaskMode, onDelete, onDone}) => {
+export const Task: FC<PropsType> = ({ showTaskMode, onDelete, onDone }) => {
   return <>
-    <Body1>{showTaskMode.task}</Body1>
+    <Card>
+      <CardBody style={{ height: '100%', alignItems: 'center' }}>
+        <CardContent style={{ height: '100%' }} cover={false}>
+          <TextBox>
+            {showTaskMode.task}
+          </TextBox>
+        </CardContent>
+      </CardBody>
+    </Card>
+    {/* <Body1></Body1> */}
     <div className={style.deleteTaskButton}>
-      <Button onClick={() => onDone(showTaskMode.id)} style={{display: 'block', marginRight: '1rem'}} view='success'>Сделано</Button>
-      <Button onClick={() => onDelete(showTaskMode.id)} style={{display: 'block', marginLeft: '1rem'}} view='critical'>Удалить</Button>
+      <Button onClick={() => onDone(showTaskMode.id)} style={{ display: 'block', marginRight: '1rem' }} view='success'>Сделано</Button>
+      <Button onClick={() => onDelete(showTaskMode.id)} style={{ display: 'block', marginLeft: '1rem' }} view='critical'>Удалить</Button>
     </div>
   </>
 }
